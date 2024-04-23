e39e2ddc68a140458116dcb8453da5dc3e09c829
anbouri <anbouri@yandex.com>
2024-04-24 00:40:07 +0100

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/GestionDesProjectsPFE/",
 
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://gestiondesprojectsserver.onrender.com",
         changeOrigin: true,
       },
     },
