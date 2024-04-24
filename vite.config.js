b57527eb471d7457ea29b3d194841dc70dec8500
KARAPAIN <88405360+KARAPAIN@users.noreply.github.com>
2024-04-24 01:28:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Adjust the output directory as needed
-  },
+  base: "/GestionDesProjectsPFE/",
 
   server: {
     port: 3000,
