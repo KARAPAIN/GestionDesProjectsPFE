433cbd1c5363eaec89d0caf4ac7f8dce32a342c4
KARAPAIN <88405360+KARAPAIN@users.noreply.github.com>
2024-05-14 20:10:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://backendpfe-v9la.onrender.com",
+        target: "https://gestiondesprojectsserver.onrender.com/",
         changeOrigin: true,
       },
     },
