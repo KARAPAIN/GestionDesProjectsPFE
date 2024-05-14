86e77f3ca5b2539ccad64a8c30d5808339cc944a
KARAPAIN <88405360+KARAPAIN@users.noreply.github.com>
2024-05-14 21:16:37 +0100

    Update authSlice.js

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,11 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Cookies from "js-cookie";
+
 
 const initialState = {
   user: localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
-  token: Cookies.get("token") || null, // Read token from cookie
+  token: localStorage.getItem("token")
+    //Cookies.get("token") || null, // Read token from cookie
   isSidebarOpen: false,
 };
 
@@ -17,13 +18,14 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token; // Store token in Redux state
       localStorage.setItem("userInfo", JSON.stringify(action.payload.user));
-      Cookies.set("token", action.payload.token, { expires: 1 });
+      localStorage.setItem("token",action.payload.token, { expires: 1 })
+     // Cookies.set("token", action.payload.token, { expires: 1 });
     },
     logout: (state, action) => {
       state.user = null;
       state.token = null; // Clear token from Redux state
       localStorage.removeItem("userInfo");
-      Cookies.remove("token");
+      localStorage.removeItem("token");
     },
     setOpenSidebar: (state, action) => {
       state.isSidebarOpen = action.payload;
